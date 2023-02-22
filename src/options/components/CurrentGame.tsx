fa9c16c7cdfa951fc6bc1ee572d3722156b01311
JMantis0 <rootmazur@gmail.com>
2023-02-22 01:46:39 -0500

    Remove unused import

--- src/options/components/CurrentGame.tsx
+++ src/options/components/CurrentGame.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import HandView from "./HandView";
 import DiscardFrame from "./DiscardView";
 import LibraryView from "./LibraryView";
@@ -9,7 +8,6 @@ import InPlayView from "./InPlayView";
 const CurrentGame = () => {
   return (
     <React.Fragment>
-      {/* <DecklistView /> */}
       <HandView />
       <br></br>
       <LibraryView />
