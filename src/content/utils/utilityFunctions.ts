256cffa91a978439fcff58489490bef4d78930a4
JMantis0 <rootmazur@gmail.com>
2023-02-22 01:44:35 -0500

    Removed duplicate function.  Is not needed here

--- src/content/utils/utilityFunctions.ts
+++ src/content/utils/utilityFunctions.ts
@@ -1,10 +1,3 @@
-import { Deck } from "../../model/deck";
-import { resetGame } from "../content";
-// import {
-//   getLogScrollContainerLogLines,
-//   getVassalPlayByComparingElementPosition,
-// } from "../contentFunctions";
-
 export type ErrorWithMessage = {
   message: string;
 };
@@ -34,62 +27,4 @@ export const toErrorWithMessage = (maybeError: unknown): ErrorWithMessage => {
 
 export const getErrorMessage = (error: unknown) => {
   return toErrorWithMessage(error).message;
-};
-
-export const appendElements = (
-  logInitialized: boolean,
-  kingdomInitialized: boolean,
-  playersInitialized: boolean,
-  playerDeckInitialized: boolean,
-  logsProcessed: string,
-  gameLog: string,
-  playerNames: Array<string>,
-  playerAbbreviatedNames: Array<string>,
-  decks: Map<string, Deck>,
-  kingdom: Array<string>,
-  treasureLine: boolean,
-  observerOn: boolean
-) => {
-  const mydiv = $("<div>").attr("id", "dev-btns").text("Dev-Buttons");
-  $(".chat-display").append(mydiv);
-  mydiv.append($("<button>").attr("id", "statebutton").text("LOG DECK STATE"));
-  mydiv.append(
-    $("<button>")
-      .text("Reset")
-      .on("click", () => {
-        resetGame();
-      })
-  );
-  mydiv.append(
-    $("<button>")
-      .attr("id", "newLogsButton")
-      .text("Console Log Globals")
-      .on("click", () => {
-        console.log("logInitialized: ", logInitialized);
-        console.log("kingdomInitialized: ", kingdomInitialized);
-        console.log("playersInitialized: ", playersInitialized);
-        console.log("playerDeckInitialized: ", playerDeckInitialized);
-        console.group("LogsProcessed Array");
-        console.log("logsProcessed: ", logsProcessed.split("\n"));
-        console.groupEnd();
-        console.group("gameLog Array");
-        console.log("gameLog: ", gameLog.split("\n"));
-        console.groupEnd();
-        console.log("playerNames: ", playerNames);
-        console.log("playerAbbreviatedNames: ", playerAbbreviatedNames);
-        console.log("decks: ", decks);
-        console.log("kingdom: ", kingdom);
-        console.log("treasureLine: ", treasureLine);
-        console.log("observerOn: ", observerOn);
-      })
-  );
-
-  mydiv.append(
-    $("<button>")
-      .attr("id", "logScrollContainer")
-      .text("Console Log getLogScrollContainer()")
-      .on("click", () => {
-        // getVassalPlayByComparingElementPosition(getLogScrollContainerLogLines());
-      })
-  );
 };
