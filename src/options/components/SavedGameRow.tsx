206e60707f78c00b3e16de66c7626dd48f368005
JMantis0 <rootmazur@gmail.com>
2023-03-18 09:14:44 -0400

    Abstracting component SavedGameRow.tsx from the HistorySelector component

--- src/options/components/SavedGameRow.tsx
+++ src/options/components/SavedGameRow.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { BaseSyntheticEvent, FunctionComponent } from "react";
+import { useDispatch } from "react-redux";
+import { SavedGame, setLogHtml } from "../../redux/optionsSlice";
 
-const SavedGameRow = () => {
+type SavedGameRowProps = {
+  savedGame: SavedGame;
+  idx: number;
+};
+
+const SavedGameRow: FunctionComponent<SavedGameRowProps> = ({
+  savedGame,
+  idx,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleRecordClick = (e: BaseSyntheticEvent, html: string) => {
+    console.log("handleRecordClick Event", e);
+    dispatch(setLogHtml(html));
+  };
   return (
-    <div>SavedGameRow</div>
-  )
-}
+    <tr
+      onClick={(e: BaseSyntheticEvent) => {
+        handleRecordClick(e, savedGame.logHtml);
+      }}
+      className="grid grid-cols-12"
+      key={idx}
+    >
+      <td className={`col-span-3 text-xs border-2 text-white`}>
+        {savedGame.playerDeck.gameTitle}
+      </td>
+      <td className={`col-span-3 text-xs border-2 text-white`}>
+        {savedGame.opponentDeck.playerName}
+      </td>
+      <td className={`col-span-3 text-xs border-2 text-white`}>
+        {savedGame.playerDeck.gameResult}
+      </td>
+      <td className={`col-span-3 text-xs border-2 text-white`}>
+        {savedGame.dateTime}
+      </td>
+    </tr>
+  );
+};
 
-export default SavedGameRow
+export default SavedGameRow;
