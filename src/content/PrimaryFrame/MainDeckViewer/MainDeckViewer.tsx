a38636b9abde0c937df5fe63b71477cbf6bf3a5f
JMantis0 <rootmazur@gmail.com>
2023-09-30 22:28:43 -0400

    Removed functions that were abstracted to TurnButton

--- src/content/PrimaryFrame/MainDeckViewer/MainDeckViewer.tsx
+++ src/content/PrimaryFrame/MainDeckViewer/MainDeckViewer.tsx
@@ -1,10 +1,5 @@
-import React, {
-  useState,
-  useEffect,
-  BaseSyntheticEvent,
-  Dispatch,
-} from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
 import {
   CardCounts,
@@ -18,51 +13,8 @@ import {
 import FullListCardRow from "./FullListCardRow/FullListCardRow";
 import MainDeckViewHeader from "./MainDeckViewHeader/MainDeckViewHeader";
 import ViewFooter from "./ViewFooter/ViewFooter";
-import {
-  setPinnedTurnToggleButton,
-  setTurnToggleButton,
-} from "../../../redux/contentSlice";
 import CustomSelect from "./MainDeckViewHeader/CustomSelect/CustomSelect";
-import { ActionCreatorWithPayload, AnyAction } from "@reduxjs/toolkit";
 import TurnButton from "./ViewFooter/TurnButton/TurnButton";
-
-const mouseLeaveTurnButton = (
-  pinnedTurnButton: "Current" | "Next",
-  dispatch: Dispatch<AnyAction>,
-  setTurn: ActionCreatorWithPayload<
-    "Current" | "Next",
-    "content/setTurnToggleButton"
-  >
-) => {
-  dispatch(setTurn(pinnedTurnButton));
-};
-
-const mouseEnterTurnButton = (
-  buttonName: "Current" | "Next",
-  dispatch: Dispatch<AnyAction>,
-  setTurnToggleButton: ActionCreatorWithPayload<
-    "Current" | "Next",
-    "content/setTurnToggleButton"
-  >
-) => {
-  dispatch(setTurnToggleButton(buttonName));
-};
-
-const turnToggleButtonClick = (
-  buttonName: "Current" | "Next",
-  dispatch: Dispatch<AnyAction>,
-  setPinnedTurnToggleButton: ActionCreatorWithPayload<
-    "Current" | "Next",
-    "content/setPinnedTurnToggleButton"
-  >,
-  setTurnToggleButton: ActionCreatorWithPayload<
-    "Current" | "Next",
-    "content/setTurnToggleButton"
-  >
-) => {
-  dispatch(setPinnedTurnToggleButton(buttonName));
-  dispatch(setTurnToggleButton(buttonName));
-};
 
 const MainDeckViewer = () => {
   const [libraryMap, setLibraryMap] = useState<Map<string, CardCounts>>(
@@ -75,14 +27,11 @@ const MainDeckViewer = () => {
   const turnToggleButton = useSelector(
     (state: RootState) => state.content.turnToggleButton
   );
- 
   const topCardsLookAmount = useSelector(
     (state: RootState) => state.content.topCardsLookAmount
   );
 
-
   useEffect(() => {
-    console.log("MainDeckViewerUseEffect");
     const unsortedCombinedMap = combineDeckListMapAndZoneListMap(
       getCountsFromArray(pd.entireDeck),
       getCountsFromArray(pd.library)
@@ -126,7 +75,6 @@ const MainDeckViewer = () => {
         <div className={"col-span-2"}>
           <TurnButton buttonName="Current" />
           <TurnButton buttonName="Next" />
-
           <CustomSelect colSpan={12} />
         </div>
       </div>
