a7cc2bcc2e6032d742c8184f6cf0a36c15fad1f3
JMantis0 <rootmazur@gmail.com>
2023-10-19 16:45:30 -0400

    Added unit tests for the 4 additions functions extracted from update method, and finally, added a unit test for the updat method itself. Coverage on deck.ts is now exceeding 90%.  I am pleased with the quality of the tests and the coverage percentage.  The uncovered portions are console logs that don't require testing.

---
+++ __tests__/model/updateArchives.test.ts
@@ -0,0 +1,76 @@
+import { describe, it, expect, jest, afterEach } from "@jest/globals";
+import { Deck } from "../../src/model/deck";
+
+describe("Function updateArchives()", () => {
+  const deck = new Deck("", false, "", "pName", "pNick", []);
+  const setLastEntryProcessed = jest.spyOn(
+    Deck.prototype,
+    "setLastEntryProcessed"
+  );
+  const addLogToLogArchive = jest.spyOn(Deck.prototype, "addLogToLogArchive");
+  const incrementTurn = jest.spyOn(Deck.prototype, "incrementTurn");
+  const checkForTurnLine = jest.spyOn(Deck.prototype, "checkForTurnLine");
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Should correctly add lines to the logArchive and lastEntryProcessed fields", () => {
+    // Arrange
+    const line = "pNick draws an Estate and 3 Coppers.";
+    // Act - simulate updating the deck logArchive and lastEntryProcessed fields with a loggable line
+    deck.updateArchives(line);
+    // Assert
+    expect(setLastEntryProcessed).toBeCalledTimes(1);
+    expect(setLastEntryProcessed).toBeCalledWith(line);
+    expect(addLogToLogArchive).toBeCalledTimes(1);
+    expect(addLogToLogArchive).toBeCalledWith(line);
+    expect(checkForTurnLine).toBeCalledTimes(1);
+    expect(checkForTurnLine).toBeCalledWith(line);
+    expect(checkForTurnLine.mock.results[0].value).toBe(false);
+    expect(incrementTurn).not.toBeCalled();
+  });
+
+  it("should correctly avoid adding Premoves lines to the logArchive and lastEntryProcessed fields", () => {
+    // Arrange
+    const line = "PremovesLog1Log2Log3";
+    // Act - simulate updating the deck logArchive and lastEntryProcessed fields with a 'Premoves' line
+    deck.updateArchives(line);
+    // Assert
+    expect(checkForTurnLine).toBeCalledTimes(1);
+    expect(checkForTurnLine).toBeCalledWith(line);
+    expect(checkForTurnLine.mock.results[0].value).toBe(false);
+    expect(setLastEntryProcessed).not.toBeCalled();
+    expect(addLogToLogArchive).not.toBeCalled();
+    expect(incrementTurn).not.toBeCalled();
+  });
+
+  it("should correctly avoid adding Between Turns lines to the logArchive and lastEntryProcessed fields", () => {
+    // Arrange
+    const line = "Between Turns";
+    // Act - simulate updating the deck logArchive and lastEntryProcessed fields with a 'Between Turns' line
+    deck.updateArchives(line);
+    // Assert
+    expect(checkForTurnLine).toBeCalledTimes(1);
+    expect(checkForTurnLine).toBeCalledWith(line);
+    expect(checkForTurnLine.mock.results[0].value).toBe(false);
+    expect(setLastEntryProcessed).not.toBeCalled();
+    expect(addLogToLogArchive).not.toBeCalled();
+    expect(incrementTurn).not.toBeCalled();
+  });
+  
+  it("should correctly Increment Turn field when processing a 'New Turn' line", () => {
+    // Arrange
+    const line = "Turn 10 - pName";
+    // Act - simulate updating the deck logArchive and lastEntryProcessed fields with a 'Between Turns' line
+    deck.updateArchives(line);
+    // Assert
+    expect(checkForTurnLine).toBeCalledTimes(1);
+    expect(checkForTurnLine).toBeCalledWith(line);
+    expect(checkForTurnLine.mock.results[0].value).toBe(true);
+    expect(incrementTurn).toBeCalledTimes(1);
+    expect(setLastEntryProcessed).toBeCalledTimes(1);
+    expect(setLastEntryProcessed).toBeCalledWith(line);
+    expect(addLogToLogArchive).toBeCalledTimes(1);
+    expect(addLogToLogArchive).toBeCalledWith(line);
+  });
+});
