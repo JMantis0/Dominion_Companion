4a5f22597aed035b569b8ef4eb771236da30cf50
JMantis0 <rootmazur@gmail.com>
2023-11-05 13:15:57 -0500

    Updating webpack scripts

--- __tests__/content/contentScript.test.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { describe, it, expect } from "@jest/globals";
-import { renderWithProviderAndCSS } from "./assets/test-utils";
-import { screen } from "@testing-library/react";
-
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,5 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "development",
-  devtool: "cheap-module-source-map",
+  devtool: "inline-source-map",
 });
