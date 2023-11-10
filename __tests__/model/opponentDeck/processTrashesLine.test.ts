756ef7816379f4168a2543f28162fdd82d1ab123
JMantis0 <rootmazur@gmail.com>
2023-11-10 00:57:45 -0500

    Improvements: Replaced portions of tests that test implementation with tests that model user behavior.

--- __tests__/model/opponentDeck/processTrashesLine.test.ts
+++ __tests__/model/opponentDeck/processTrashesLine.test.ts
@@ -1,39 +1,27 @@
-import { describe, it, expect, jest, afterEach } from "@jest/globals";
+import { describe, it, expect,  beforeEach } from "@jest/globals";
 import { OpponentDeck } from "../../../src/model/opponentDeck";
 
-describe("Method processTrashesLine", () => {
-  let deck = new OpponentDeck("", false, "", "oName", "oNick", []);
-  // Spy on method dependencies
-  const setTrash = jest.spyOn(OpponentDeck.prototype, "setTrash");
-  const removeCardFromEntireDeck = jest.spyOn(
-    OpponentDeck.prototype,
-    "removeCardFromEntireDeck"
-  );
-  afterEach(() => {
-    jest.clearAllMocks();
+describe("processTrashesLine", () => {
+  // Declare OpponentDeck reference.
+  let deck: OpponentDeck;
+
+  beforeEach(() => {
     deck = new OpponentDeck("", false, "", "oName", "oNick", []);
   });
+
   it("should correctly add the given cards to trash and remove them from the entireDeck", () => {
     // Arrange sample trash and entireDeck zones
     deck.trash = ["Copper"];
-    deck.entireDeck = ["Copper", "Copper", "Estate", "Estate", "Moneylender"];
+    deck.entireDeck = ["Copper", "Copper", "Estate", "Estate", "Market"];
     // Act - Simulate opponent trashing 4 cards with a Chapel
-    deck.processTrashesLine(["Copper", "Estate", "Moneylender"], [1, 2, 1]);
-    expect(setTrash).toBeCalledTimes(4);
-    expect(setTrash).nthCalledWith(1, ["Copper", "Copper"]);
-    expect(setTrash).nthCalledWith(2, ["Copper", "Copper", "Estate"]);
-    expect(setTrash).nthCalledWith(3, ["Copper", "Copper", "Estate", "Estate"]);
-    expect(setTrash).nthCalledWith(4, [
+    deck.processTrashesLine(["Copper", "Estate"], [2, 2]);
+    expect(deck.trash).toStrictEqual([
+      "Copper",
       "Copper",
       "Copper",
       "Estate",
       "Estate",
-      "Moneylender",
     ]);
-    expect(removeCardFromEntireDeck).toBeCalledTimes(4);
-    expect(removeCardFromEntireDeck).nthCalledWith(1, "Copper");
-    expect(removeCardFromEntireDeck).nthCalledWith(2, "Estate");
-    expect(removeCardFromEntireDeck).nthCalledWith(3, "Estate");
-    expect(removeCardFromEntireDeck).nthCalledWith(4, "Moneylender");
+    expect(deck.entireDeck).toStrictEqual(["Market"]);
   });
 });
