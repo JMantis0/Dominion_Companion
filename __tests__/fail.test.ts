238699d0fe38fe923d23bd57768f47c07e4c8774
JMantis0 <rootmazur@gmail.com>
2023-12-02 14:43:10 -0500

    Remove reporter and failing test.

--- __tests__/fail.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "@jest/globals";
-describe("Fail!", () => {
-  it("Should pass", () => {
-    expect(true).toBe(true);
-  });
-  it("Should fail2", () => {
-    expect(false).toBe(true);
-  });
-});
