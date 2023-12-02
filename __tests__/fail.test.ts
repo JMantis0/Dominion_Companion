35e6dba05ec45716259b604c713293479eba8bb5
JMantis0 <rootmazur@gmail.com>
2023-12-02 13:58:49 -0500

    Trying another failing push

--- __tests__/fail.test.ts
+++ __tests__/fail.test.ts
@@ -1,7 +1,9 @@
 import { describe, it, expect } from "@jest/globals";
-
 describe("Fail!", () => {
-  it("Should fail", () => {
+  it("Should pass", () => {
+    expect(true).toBe(true);
+  });
+  it("Should fail2", () => {
     expect(false).toBe(true);
   });
 });
